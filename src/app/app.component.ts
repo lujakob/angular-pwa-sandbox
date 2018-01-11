7fa8129461643c2e79061b5f92477b9bc97db2c9
Lukas Jakob <lu.jakob@googlemail.com>
2018-01-11 10:17:06 +0100

    Add reset style. Add mat menu icon and refactor to constants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {DomSanitizer} from '@angular/platform-browser';
-import {MatIconRegistry} from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
+import {SVG_ICONS} from '../constants'
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,14 @@ export class AppComponent {
   title = 'app';
   links = [{path: '/', title: 'Home'}, {path: 'repos', title: 'Repositories'}];
 
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon(
-      'thumbs-up',
-      sanitizer.bypassSecurityTrustResourceUrl('assets/img/examples/thumbup-icon.svg'));
+  constructor(private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer) {
+    this.registerSvgIcons();
+  }
+
+  registerSvgIcons() {
+    SVG_ICONS.map(item => this.iconRegistry.addSvgIcon(
+      item.name,
+      this.sanitizer.bypassSecurityTrustResourceUrl(item.path)
+    ))
   }
 }
---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const SVG_ICONS = [
+  {
+    name: 'menu',
+    path: 'assets/svg/ic_menu_black_24px.svg'
+  }
+];
